680b21bfbd287fbb80e1f581ea4a4a55a0246e38
Rencus <81608004+Rencus@users.noreply.github.com>
2021-10-28 17:09:09 +0700

    CSS

--- form_tambah.php
+++ form_tambah.php
@@ -2,34 +2,37 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <title>GENSHIN IMPACT</title>
 
-    <h1><center>TAMBAH DATA</center></h1><</br>
+   
+<div class="container my-2">
 
-<div class="container">
+<h1><center>TAMBAH DATA</center></h1></br>
+
 <form method = "POST" action = "tambah.php">
   <div class="mb-3">
     <label class="form-label">ID</label>
-    <input type="text" class="form-control" name = "id">
+    <input type="text" class="form-control" name = "id" required>
   </div>
   <div class="mb-3">
     <label class="form-label">Nama</label>
-    <input type="text" class="form-control" name = "nama">
+    <input type="text" class="form-control" name = "nama" required>
   </div>
   <div class="mb-3">
     <label class="form-label">Elemen</label>
-    <input type="text" class="form-control" name = "elemen">
+    <input type="text" class="form-control" name = "elemen" required>
   </div>
   <div class="mb-3">
     <label class="form-label">TTL</label>
-    <input type="date" class="form-control" name = "ttl">
+    <input type="date" class="form-control" name = "ttl" required>
   </div>
   <div class="mb-3">
     <label class="form-label">Kota</label>
-    <input type="text" class="form-control" name = "kota">
+    <input type="text" class="form-control" name = "kota" required>
   </div>
   <button type="submit" class="btn btn-primary" value = "simpan">Submit</button>
+  <a class="btn btn-success" href = "index.php">Lihat data</a>
 </form>
 </div>
-    <center><a href = "index.php">Lihat data</a></center>
+    
 </form>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,42 +1,55 @@
 <html>
 <title>GENSHIN IMPACT</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<body class="text-light" background="../bg1.jpg">
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+
+<body>
     <center>
-        <h1>CHARA GENSHIN</h1></br>
+       
 
             <table border = "1" cellpadding = "5" cellspacing = "0">
-        <nav>
-        <a href = "tambah.php">Tambah Data</a>
-</nav>
+       
 <br>
-    <tr>
-        <th>ID</th>
-        <th>Nama</th>
-        <th>Elemen</th>
-        <th>TTL</th>
-        <th>Kota</th>
-        <th>Opsi</th>
-    </tr>
+</body>
+    </table>
+    <div class="container my-2">
+    <h1 >CHARA GENSHIN</h1></br>
+    <nav class="my-5">
+        <a href = "tambah.php" class="btn btn-success"><span><i class="bi bi-file-plus"></i></i> Tambah Data</a>
+    </nav>
 
-<?php include("koneksi.php");
+    <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nama</th>
+      <th scope="col">Elemen</th>
+      <th scope="col">TTL</th>
+      <th scope="col">Kota</th>
+      <th scope="col">Opsi</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php include("koneksi.php");
 if($conn){
 $hasil = mysqli_query($conn,"SELECT * FROM data_character");
 while ($row = mysqli_fetch_array($hasil))
 {
-echo "<tr><td align=center>$row[id]</td>
-      <td align=center>$row[nama]</td>
-      <td align=center>$row[elemen]</td>
-      <td align=center>$row[ttl]</td>
-      <td align=center>$row[kota]</td>
-      <td align=center><a href=form_ubah.php?id=$row[id]>Ubah</a>
-      <a href=hapus.php?id=$row[id]>Hapus</a>
+echo "<tr><td>$row[id]</td>
+      <td>$row[nama]</td>
+      <td>$row[elemen]</td>
+      <td>$row[ttl]</td>
+      <td>$row[kota]</td>
+      <td><a class='btn btn-primary' href=form_ubah.php?id=$row[id]>Ubah</a>
+      <a class='btn btn-danger' href=hapus.php?id=$row[id]>Hapus</a>
       </tr>";
 
     $row=mysqli_query($conn,"select * from data_character where id='$row[id]'");
 }
 }
 ?>
-</body>
+  </tbody>
 </table>
+    </div>
 </html>
